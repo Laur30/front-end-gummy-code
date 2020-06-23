010b4dfaf452e4a83560dc162cb75beec7a37acf
Laur30 <laurwints@gmail.com>
2020-06-22 20:54:25 -0500

    setup purchase history component

--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+import PageTitle from '../pageTitle';
+
 class PurchaseHistory extends Component {
     render() {
         return (
-            <div>
-                purchase history
+            <div className='purcahe-history'>
+                <PageTitle className='purchase-history__page-title' title='Purchase History'/>
             </div>
         )
     }
